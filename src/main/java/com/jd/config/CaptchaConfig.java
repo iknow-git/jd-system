dace2007a5352f6b7f87f211670142ee3526d5aa
iknow-git <113151136+iknow-git@users.noreply.github.com>
2024-10-31 21:32:06 +0800

    增加PC端页面

--- src/main/java/com/jd/config/CaptchaConfig.java
+++ src/main/java/com/jd/config/CaptchaConfig.java
@@ -19,6 +19,7 @@ public class CaptchaConfig
     public DefaultKaptcha getKaptchaBean()
     {
         
+        
         DefaultKaptcha defaultKaptcha = new DefaultKaptcha();
         Properties properties = new Properties();
         // 是否有边框 默认为true 我们可以自己设置yes，no
