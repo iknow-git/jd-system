3d3af9baf9c8adbee68d98a2284535284856a744
iknow-git <113151136+iknow-git@users.noreply.github.com>
2024-11-27 20:39:14 +0800

    add pc

--- src/main/java/com/jd/config/KaptchaTextCreator.java
+++ src/main/java/com/jd/config/KaptchaTextCreator.java
@@ -11,6 +11,7 @@ import com.google.code.kaptcha.text.impl.DefaultTextCreator;
 public class KaptchaTextCreator extends DefaultTextCreator
 {
     private static final String[] CNUMBERS = "0,1,2,3,4,5,6,7,8,9,10".split(",");
+    
 
     @Override
     public String getText()
