b131659787c4f1a3c97394a9bf5d870fe92e4345
iknow-git <113151136+iknow-git@users.noreply.github.com>
2024-12-12 16:27:15 +0800

    返回zset分数

--- src/main/java/com/jd/service/impl/RedissonClientUtil.java
+++ src/main/java/com/jd/service/impl/RedissonClientUtil.java
@@ -24,3 +24,16 @@
         memberList.forEach(member -> batch.getScoredSortedSet(key).removeAsync(member));
         batch.execute();
     }
+
+
+ /**
+     * 返回ZSet分数范围内 member 列表. 区间包含分数本身.
+     * 注意这里不能用 -1 代替最大值
+     */
+    public Collection<Object> getZSetMembersByScoresInclusive(String key, double startScore, double endScore) {
+        RScoredSortedSet<Object> scoredSortedSet = client.getScoredSortedSet(key);
+        if (!scoredSortedSet.isExists()) {
+            return null;
+        }
+        return scoredSortedSet.valueRange(startScore, true, endScore, true);
+    }
