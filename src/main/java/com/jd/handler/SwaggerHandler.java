ea5551bf99ca8bdc34dfab09baffb9799f6fe577
iknow-git <113151136+iknow-git@users.noreply.github.com>
2025-02-28 08:40:11 +0800

    swaggerResources

--- src/main/java/com/jd/handler/SwaggerHandler.java
+++ src/main/java/com/jd/handler/SwaggerHandler.java
@@ -53,4 +53,31 @@ public class SwaggerHandler
     {
         return Mono.just((new ResponseEntity<>(swaggerResources.get(), HttpStatus.OK)));
     }
+
+    
+    @Override
+    public User action(String object, Map<String, WorkerWrapper> allWrappers) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return new User("user0");
+    }
+
+
+    @Override
+    public User defaultValue() {
+        return new User("default User");
+    }
+
+    @Override
+    public void begin() {
+        //System.out.println(Thread.currentThread().getName() + "- start --" + System.currentTimeMillis());
+    }
+
+    @Override
+    public void result(boolean success, String param, WorkResult<User> workResult) {
+        System.out.println("worker0 的结果是：" + workResult.getResult());
+    }
 }
