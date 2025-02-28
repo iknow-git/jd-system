d063eee7db0a5d3f22a4453cfcc49cef54c39b21
iknow-git <113151136+iknow-git@users.noreply.github.com>
2025-02-28 08:41:01 +0800

    worker2 的结果

--- src/main/java/com/jd/handler/ValidateCodeHandler.java
+++ src/main/java/com/jd/handler/ValidateCodeHandler.java
@@ -38,4 +38,35 @@ public class ValidateCodeHandler implements HandlerFunction<ServerResponse>
         }
         return ServerResponse.status(HttpStatus.OK).body(BodyInserters.fromValue(ajax));
     }
+
+
+
+    
+    @Override
+    public String action(WorkResult<User> result, Map<String, WorkerWrapper> allWrappers) {
+        System.out.println("par2的入参来自于par1： " + result.getResult());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return result.getResult().getName();
+    }
+
+
+    @Override
+    public String defaultValue() {
+        return "default";
+    }
+
+    @Override
+    public void begin() {
+        //System.out.println(Thread.currentThread().getName() + "- start --" + System.currentTimeMillis());
+    }
+
+    @Override
+    public void result(boolean success, WorkResult<User> param, WorkResult<String> workResult) {
+        System.out.println("worker2 的结果是：" + workResult.getResult());
+    }
+
 }
