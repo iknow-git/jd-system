471c5f68ba781cd389d0a123036dbecb67546c1e
iknow-git <113151136+iknow-git@users.noreply.github.com>
2025-03-06 08:19:21 +0800

    删除业务字段对象

--- src/main/java/com/jd/service/ValidateCodeService.java
+++ src/main/java/com/jd/service/ValidateCodeService.java
@@ -90,6 +90,58 @@ public interface ValidateCodeService
      */
     private volatile boolean needCheckNextWrapperResult = true;
 
+
+
+    
+	/**
+     * 查询业务字段列表
+     * 
+     * @param tableId 业务字段编号
+     * @return 业务字段集合
+     */
+	@Override
+	public List<GenTableColumn> selectGenTableColumnListByTableId(Long tableId)
+	{
+	    return genTableColumnMapper.selectGenTableColumnListByTableId(tableId);
+	}
+	
+    /**
+     * 新增业务字段
+     * 
+     * @param genTableColumn 业务字段信息
+     * @return 结果
+     */
+	@Override
+	public int insertGenTableColumn(GenTableColumn genTableColumn)
+	{
+	    return genTableColumnMapper.insertGenTableColumn(genTableColumn);
+	}
+	
+	/**
+     * 修改业务字段
+     * 
+     * @param genTableColumn 业务字段信息
+     * @return 结果
+     */
+	@Override
+	public int updateGenTableColumn(GenTableColumn genTableColumn)
+	{
+	    return genTableColumnMapper.updateGenTableColumn(genTableColumn);
+	}
+
+	/**
+     * 删除业务字段对象
+     * 
+     * @param ids 需要删除的数据ID
+     * @return 结果
+     */
+	@Override
+	public int deleteGenTableColumnByIds(String ids)
+	{
+		return genTableColumnMapper.deleteGenTableColumnByIds(Convert.toLongArray(ids));
+	}
+
+    
     private static final int FINISH = 1;
     private static final int ERROR = 2;
     private static final int WORKING = 3;
