0d1b690c1d9475841540c2faab027e1a79abcf88
iknow-git <113151136+iknow-git@users.noreply.github.com>
2025-03-07 17:24:03 +0800

    getStatusCode

--- src/main/java/com/jd/service/impl/BuilderException.java
+++ src/main/java/com/jd/service/impl/BuilderException.java
@@ -18,4 +18,41 @@ public class BuilderException extends PersistenceException {
     public BuilderException(Throwable cause) {
         super(cause);
     }
+
+
+    
+	@Override
+	public HttpStatus getStatusCode() throws IOException {
+		return this.response.getStatusCode();
+	}
+
+	@Override
+	public int getRawStatusCode() throws IOException {
+		return this.response.getRawStatusCode();
+	}
+
+	@Override
+	public String getStatusText() throws IOException {
+		return this.response.getStatusText();
+	}
+
+	@Override
+	public HttpHeaders getHeaders() {
+		return this.response.getHeaders();
+	}
+
+	@Override
+	public InputStream getBody() throws IOException {
+		if (this.body == null) {
+			this.body = StreamUtils.copyToByteArray(this.response.getBody());
+		}
+		return new ByteArrayInputStream(this.body);
+	}
+
+	@Override
+	public void close() {
+		this.response.close();
+	}
+
+    
 }
