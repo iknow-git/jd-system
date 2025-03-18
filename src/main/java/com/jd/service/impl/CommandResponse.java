71d572723e84965e6afc464a12c61d8d36a00631
iknow-git <113151136+iknow-git@users.noreply.github.com>
2025-03-18 09:02:39 +0800

    将前台传递过来的日期格式的字符串，自动转化为Date类型

--- src/main/java/com/jd/service/impl/CommandResponse.java
+++ src/main/java/com/jd/service/impl/CommandResponse.java
@@ -83,8 +83,60 @@ public class CommandResponse {
      */
     public int interval() default 5000;
 
+ private final byte[] body;
 
-    
+    public RepeatedlyRequestWrapper(HttpServletRequest request, ServletResponse response) throws IOException
+    {
+        super(request);
+        request.setCharacterEncoding(Constants.UTF8);
+        response.setCharacterEncoding(Constants.UTF8);
+
+        body = HttpHelper.getBodyString(request).getBytes(Constants.UTF8);
+    }
+
+    @Override
+    public BufferedReader getReader() throws IOException
+    {
+        return new BufferedReader(new InputStreamReader(getInputStream()));
+    }
+
+    @Override
+    public ServletInputStream getInputStream() throws IOException
+    {
+        final ByteArrayInputStream bais = new ByteArrayInputStream(body);
+        return new ServletInputStream()
+        {
+            @Override
+            public int read() throws IOException
+            {
+                return bais.read();
+            }
+
+            @Override
+            public int available() throws IOException
+            {
+                return body.length;
+            }
+
+            @Override
+            public boolean isFinished()
+            {
+                return false;
+            }
+
+            @Override
+            public boolean isReady()
+            {
+                return false;
+            }
+
+            @Override
+            public void setReadListener(ReadListener readListener)
+            {
+
+            }
+        };
+    }
     /**
      * 将前台传递过来的日期格式的字符串，自动转化为Date类型
      */
